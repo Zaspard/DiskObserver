90c8f1296c911b97f44209f3951b3dce9594d550
Alexander Musienko <aalex96@mail.ru>
2022-10-09 20:41:34 +0700

    add interface and model for directory + file

---
+++ DiskObserver/Model/Interface/IDirectory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskObserver.Model.Interface {
+    public interface IDirectory : IPhysicalObject {
+        public string Name { get; set; }
+        public bool IsHidden { get; }
+        public long Size { get; }
+
+        public ObservableCollection<IPhysicalObject> PhysicalObjects { get; set; }
+    }
+}
---
+++ DiskObserver/Model/Interface/IFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskObserver.Model.Interface {
+    public interface IFile : IPhysicalObject {
+        public string Name { get; set; }
+        public bool IsHidden { get; }
+        public long Size { get; }
+        public string Format { get; }
+    }
+}
