05e9e535893417b9d4008ba809e8f081c0523716
Alexander Musienko <aalex96@mail.ru>
2022-10-09 19:06:46 +0700

    add DiskModel + load info from DriveInfo + some UI

---
+++ DiskObserver/Model/PhysicalDisk.cs
@@ -0,0 +1,81 @@
+﻿using DiskObserver.Utils;
+using System;
+using System.IO;
+
+#nullable enable
+
+namespace DiskObserver.Model {
+    public sealed class PhysicalDisk : BaseModel, IDisposable {
+
+        string _name = "";
+        public string Name {
+            get => _name;
+            private set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        string _format = "";
+        public string Format {
+            get => _format;
+            private set
+            {
+                _format = value;
+                OnPropertyChanged(nameof(Format));
+            }
+        }
+
+        private long _totalMemory;
+        public long TotalMemory {
+            get => _totalMemory;
+            private set
+            {
+                _totalMemory = value;
+                OnPropertyChanged(nameof(TotalMemory));
+            }
+        }
+
+        private long _freeMemory;
+        public long FreeMemory {
+            get => _freeMemory;
+            private set
+            {
+                _freeMemory = value;
+                OnPropertyChanged(nameof(FreeMemory));
+            }
+        }
+
+        private bool _isNotLoad;
+        public bool IsNotReady {
+            get => _isNotLoad;
+            private set
+            {
+                _isNotLoad = value;
+                OnPropertyChanged(nameof(IsNotReady));
+            }
+        }
+
+
+        private DriveInfo _driveInfo;
+        public PhysicalDisk(DriveInfo driveInfo) {
+
+            _driveInfo = driveInfo;
+            Name = driveInfo.Name;
+            if (driveInfo.IsReady) {
+                Format = driveInfo.DriveFormat;
+                TotalMemory = driveInfo.TotalSize;
+                FreeMemory = driveInfo.AvailableFreeSpace;
+            }
+        }
+
+        public void Dispose() {
+            _driveInfo = null;
+        }
+
+        public void RefreshProperty() {
+            FreeMemory = _driveInfo.AvailableFreeSpace;
+        }
+    }
+}
--- DiskObserver/ViewModels/DiskObserverVM.cs
+++ DiskObserver/ViewModels/DiskObserverVM.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DiskObserver.Model;
+using DiskObserver.Utils;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
 
 namespace DiskObserver.ViewModels {
-    public sealed class DiskObserverVM {
+    public sealed class DiskObserverVM : BaseModel, IDisposable {
+
+        public ObservableCollection<PhysicalDisk> PhysicalDisks { get; set; } = new();
+
+        public DiskObserverVM() {
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo driveInfo in drives) {
+                PhysicalDisks.Add(new(driveInfo));
+            }
+        }
+
+        public void Dispose() {
+            foreach (PhysicalDisk physicalDisk in PhysicalDisks)
+                physicalDisk.Dispose();
+
+            PhysicalDisks.Clear();
+        }
+
+        public void Refrech() {
+
+        }
     }
 }
