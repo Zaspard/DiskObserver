313f91d889b701f98cf988a41386ede28c8bf558
Alexander Musienko <aalex96@mail.ru>
2022-10-23 16:41:04 +0700

    add more unit test

---
+++ DiskObserver.Tests/FindAllHeavyFiles.cs
@@ -0,0 +1,29 @@
+﻿using DiskObserver.Model.Implementation;
+using DiskObserver.Model.Interface;
+using NUnit.Framework;
+using System.IO;
+using System.Reflection;
+
+namespace DiskObserver.Tests {
+    public class FindAllHeavyFiles {
+
+        [Test]
+        public void CheckTestFolder1Model() {
+            string path = Assembly.GetExecutingAssembly().Location;
+            Assert.IsNotNull(path, "Assert path is null");
+
+            DirectoryInfo directoryInfo = Directory.GetParent(path);
+            Assert.IsNotNull(directoryInfo, "DirectoryInfo path is null");
+
+            DirectoryModel directoryModel = new(directoryInfo, null);
+            Assert.IsNotNull(directoryModel, "DirectoryModel path is null");
+
+            directoryModel.LazyInit();
+            Assert.NotZero(directoryModel.PhysicalObjects.Count, "DirectoryModel have 0 items");
+
+            //directoryModel.GetHeavyFiles();
+            //IPhysicalObject fileModel = directoryModel.PhysicalObjects.FirstOrDefault(x => x.Path == path);
+            //Assert.IsNotNull(path, "DirectoryModel not contains file");
+        }
+    }
+}
