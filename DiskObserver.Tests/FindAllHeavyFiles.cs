6b90a11aa5d1f274f8b466b9b6c8cd001df964a2
Alexander Musienko <aalex96@mail.ru>
2022-10-23 17:08:33 +0700

    CheckTestFolderModel

--- DiskObserver.Tests/FindAllHeavyFiles.cs
+++ DiskObserver.Tests/FindAllHeavyFiles.cs
@@ -1,6 +1,7 @@
 ﻿using DiskObserver.Model.Implementation;
 using DiskObserver.Model.Interface;
 using NUnit.Framework;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 
@@ -8,11 +9,18 @@ namespace DiskObserver.Tests {
     public class FindAllHeavyFiles {
 
         [Test]
-        public void CheckTestFolder1Model() {
+        [TestCase("TestFolder1", 62)]
+        [TestCase("TestFolder2", 150)]
+        [TestCase("TestFolder3", 123)]
+        public void CheckTestFolderModel(string nameFolder, long sizeFile) {
             string path = Assembly.GetExecutingAssembly().Location;
             Assert.IsNotNull(path, "Assert path is null");
+            path = Path.Combine(Path.GetDirectoryName(path), "TestFiles", nameFolder);
+            Assert.IsNotNull(path, "Assert path is null");
 
-            DirectoryInfo directoryInfo = Directory.GetParent(path);
+            Assert.IsTrue(Directory.Exists(path), "Not found directory with test files");
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
             Assert.IsNotNull(directoryInfo, "DirectoryInfo path is null");
 
             DirectoryModel directoryModel = new(directoryInfo, null);
@@ -21,9 +29,11 @@ namespace DiskObserver.Tests {
             directoryModel.LazyInit();
             Assert.NotZero(directoryModel.PhysicalObjects.Count, "DirectoryModel have 0 items");
 
-            //directoryModel.GetHeavyFiles();
-            //IPhysicalObject fileModel = directoryModel.PhysicalObjects.FirstOrDefault(x => x.Path == path);
-            //Assert.IsNotNull(path, "DirectoryModel not contains file");
+            List<IFile> files = new();
+            directoryModel.GetHeavyFiles(files, 3);
+
+            Assert.NotZero(files.Count, "Can't find files in directory");
+            Assert.AreEqual(files[0].Size, sizeFile);
         }
     }
 }
