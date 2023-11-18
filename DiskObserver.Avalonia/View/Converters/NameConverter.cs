238ced85ebccf0bb5f117c4b2117cb2963fd1a39
Alexander Musienko <aalex96@mail.ru>
2023-11-18 16:50:27 +0700

    Bruh

---
+++ DiskObserver.Avalonia/View/Converters/NameConverter.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using Avalonia.Data.Converters;
+using DiskObserver.Avalonia.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace DiskObserver.Avalonia.View.Converters {
+    public class NameConverter : IValueConverter {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+
+            if (value is IEnumerable<IPhysicalObject> collection) {
+                return collection.OrderBy(x => x.Name);
+            }
+
+            return AvaloniaProperty.UnsetValue;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
